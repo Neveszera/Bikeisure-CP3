4e5b3b49349edf8839c6256463c92a46aec559f1
Neveszera <gabrielgng26@gmail.com>
2023-05-21 23:21:09 -0300

    lógica mudança de modo

--- script/cp3.js
+++ script/cp3.js
@@ -1,148 +1,152 @@
 (function () {
-    var form = document.getElementById("myForm");
-    var primeiroNomeInput = document.getElementById("primeiro-nome");
-    var segundoNomeInput = document.getElementById("segundo-nome");
-    var emailInput = document.getElementById("email");
-    var senhaInput = document.getElementById("senha");
-    var confirmarSenhaInput = document.getElementById("confirmar-senha");
-  
-    form.addEventListener("submit", function (event) {
-      event.preventDefault();
-      validateForm();
-    });
-  
-    form.addEventListener("input", function (event) {
-      var target = event.target;
-      if (target.tagName.toLowerCase() === "input") {
-        removeError(target);
-      }
-    });
-  
-    function validateForm() {
-      var primeiroNome = primeiroNomeInput.value;
-      var segundoNome = segundoNomeInput.value;
-      var email = emailInput.value;
-      var senha = senhaInput.value;
-      var confirmarSenha = confirmarSenhaInput.value;
-  
-      var isValid = true;
-  
-      if (!isInputValid(primeiroNome, 5) || primeiroNome.trim() === "") {
-        showError(primeiroNomeInput, "Primeiro nome inválido. Deve ter no mínimo 5 caracteres.");
-        isValid = false;
-      }
-  
-      if (!isInputValid(segundoNome, 5) || segundoNome.trim() === "") {
-        showError(segundoNomeInput, "Segundo nome inválido. Deve ter no mínimo 5 caracteres.");
-        isValid = false;
-      }
-  
-      if (!isInputValid(email, 5) || email.trim() === "" || !email.includes("@")) {
-        showError(emailInput, "Email inválido. Deve ter no mínimo 5 caracteres e conter o caractere '@'.");
-        isValid = false;
-      }
-  
-      if (!isInputValid(senha, 6, 8) || senha.trim() === "") {
-        showError(senhaInput, "Senha inválida. Deve ter no mínimo 6 caracteres e no máximo 8 caracteres.");
-        isValid = false;
-      }
-  
-      if (senha !== confirmarSenha) {
-        showError(confirmarSenhaInput, "A confirmação de senha não corresponde à senha digitada.");
-        isValid = false;
-      }
-  
-      if (isValid) {
-        form.submit();
-      }
-    }
-  
-    function isInputValid(value, minLength, maxLength) {
-      value = value.trim();
-      if (maxLength !== undefined) {
-        return value.length >= minLength && value.length <= maxLength;
-      }
-      return value.length >= minLength;
-    }
-  
-    function showError(inputElement, errorMessage) {
-      inputElement.classList.add("error");
-  
-      var errorElementId = inputElement.id + "-error";
-      var errorElement = document.getElementById(errorElementId);
-  
-      if (errorElement) {
-        errorElement.textContent = errorMessage;
-      }
-    }
-  
-    function removeError(inputElement) {
-      inputElement.classList.remove("error");
-  
-      var errorElementId = inputElement.id + "-error";
-      var errorElement = document.getElementById(errorElementId);
-  
-      if (errorElement) {
-        errorElement.textContent = "";
-      }
-    }
-  })();
+  var form = document.getElementById("myForm");
+  var primeiroNomeInput = document.getElementById("primeiro-nome");
+  var segundoNomeInput = document.getElementById("segundo-nome");
+  var emailInput = document.getElementById("email");
+  var senhaInput = document.getElementById("senha");
+  var confirmarSenhaInput = document.getElementById("confirmar-senha");
 
-  // Dark Mode
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    validateForm();
+  });
 
-  (function () {
-    var form = document.getElementById("myForm");
-    var body = document.body;
-  
-    form.addEventListener("submit", function (event) {
-      event.preventDefault();
-      validateForm();
-    });
-  
-    form.addEventListener("input", function (event) {
-      var target = event.target;
-      if (target.tagName.toLowerCase() === "input") {
-        removeError(target);
-      }
-    });
-  
-    function validateForm() {
-      // ... código de validação do formulário ...
+  form.addEventListener("input", function (event) {
+    var target = event.target;
+    if (target.tagName.toLowerCase() === "input") {
+      removeError(target);
     }
-  
-    function showError(inputElement, errorMessage) {
-      inputElement.classList.add("error");
-  
-      var errorElementId = inputElement.id + "-error";
-      var errorElement = document.getElementById(errorElementId);
-  
-      if (errorElement) {
-        errorElement.textContent = errorMessage;
-      }
+  });
+
+  function validateForm() {
+    var primeiroNome = primeiroNomeInput.value;
+    var segundoNome = segundoNomeInput.value;
+    var email = emailInput.value;
+    var senha = senhaInput.value;
+    var confirmarSenha = confirmarSenhaInput.value;
+
+    var isValid = true;
+
+    if (!isInputValid(primeiroNome, 5) || primeiroNome.trim() === "") {
+      showError(primeiroNomeInput, "Primeiro nome inválido. Deve ter no mínimo 5 caracteres.");
+      isValid = false;
     }
-  
-    function removeError(inputElement) {
-      inputElement.classList.remove("error");
-  
-      var errorElementId = inputElement.id + "-error";
-      var errorElement = document.getElementById(errorElementId);
-  
-      if (errorElement) {
-        errorElement.textContent = "";
-      }
+
+    if (!isInputValid(segundoNome, 5) || segundoNome.trim() === "") {
+      showError(segundoNomeInput, "Segundo nome inválido. Deve ter no mínimo 5 caracteres.");
+      isValid = false;
     }
-  
-    function toggleDarkMode() {
-      body.classList.toggle("dark-mode");
+
+    if (!isInputValid(email, 5) || email.trim() === "" || !email.includes("@")) {
+      showError(emailInput, "Email inválido. Deve ter no mínimo 5 caracteres e conter o caractere '@'.");
+      isValid = false;
     }
-  
-    var darkModeButton = document.createElement("button");
-    darkModeButton.textContent = "Modo Escuro";
-    darkModeButton.addEventListener("click", toggleDarkMode);
-  
-    form.appendChild(darkModeButton);
-  
-    // Adicionando a classe dark-mode ao body
-    body.classList.add("dark-mode");
-  })();
-  
+
+    if (!isInputValid(senha, 6, 8) || senha.trim() === "") {
+      showError(senhaInput, "Senha inválida. Deve ter no mínimo 6 caracteres e no máximo 8 caracteres.");
+      isValid = false;
+    }
+
+    if (senha !== confirmarSenha) {
+      showError(confirmarSenhaInput, "A confirmação de senha não corresponde à senha digitada.");
+      isValid = false;
+    }
+
+    if (isValid) {
+      form.submit();
+    }
+  }
+
+  function isInputValid(value, minLength, maxLength) {
+    value = value.trim();
+    if (maxLength !== undefined) {
+      return value.length >= minLength && value.length <= maxLength;
+    }
+    return value.length >= minLength;
+  }
+
+  function showError(inputElement, errorMessage) {
+    inputElement.classList.add("error");
+
+    var errorElementId = inputElement.id + "-error";
+    var errorElement = document.getElementById(errorElementId);
+
+    if (errorElement) {
+      errorElement.textContent = errorMessage;
+    }
+  }
+
+  function removeError(inputElement) {
+    inputElement.classList.remove("error");
+
+    var errorElementId = inputElement.id + "-error";
+    var errorElement = document.getElementById(errorElementId);
+
+    if (errorElement) {
+      errorElement.textContent = "";
+    }
+  }
+})();
+
+// Dark Mode
+
+var toggleModeInput = document.getElementById("toggle-mode");
+toggleModeInput.addEventListener("change", function () {
+  document.body.classList.toggle("dark-mode");
+});
+
+(function () {
+  var form = document.getElementById("myForm");
+  var body = document.body;
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    validateForm();
+  });
+
+  form.addEventListener("input", function (event) {
+    var target = event.target;
+    if (target.tagName.toLowerCase() === "input") {
+      removeError(target);
+    }
+  });
+
+  function validateForm() {
+    // ... código de validação do formulário ...
+  }
+
+  function showError(inputElement, errorMessage) {
+    inputElement.classList.add("error");
+
+    var errorElementId = inputElement.id + "-error";
+    var errorElement = document.getElementById(errorElementId);
+
+    if (errorElement) {
+      errorElement.textContent = errorMessage;
+    }
+  }
+
+  function removeError(inputElement) {
+    inputElement.classList.remove("error");
+
+    var errorElementId = inputElement.id + "-error";
+    var errorElement = document.getElementById(errorElementId);
+
+    if (errorElement) {
+      errorElement.textContent = "";
+    }
+  }
+
+  function toggleDarkMode() {
+    body.classList.toggle("dark-mode");
+  }
+
+  var darkModeButton = document.createElement("button");
+  darkModeButton.textContent = "Modo Escuro";
+  darkModeButton.addEventListener("click", toggleDarkMode);
+
+  form.appendChild(darkModeButton);
+
+  // Adicionando a classe dark-mode ao body
+  body.classList.add("dark-mode");
+})();
